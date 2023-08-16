41a39c8953dc78f52429a7e370de8683f80cfe7e
msametyildiz <97543064+msametyildiz@users.noreply.github.com>
2023-08-16 23:49:52 +0300

    Added the new  part

--- lib/mainLectures.dart
+++ lib/mainLectures.dart
@@ -575,5 +575,57 @@ class _MyHomePageState extends State<MyHomePage> {
 ----------------------------------------------------            ----------------------------------------------------------------------
 
 
+// ---------------------------------------------  Try-Catch Kullanımı   -----------------------------------------------------------------------------
+
+  derleme sırasında oluşabilecek hatalar için kullanılır .
+
+  try {
+      kontrol edilecek kodlar buraya yazılmalır
+  }catch(e){
+      hata oluşunca burası çalışır
+  }
+
+
+  var sayilar = <int>[];
+  sayilar.add(12);  //0.index
+  sayilar.add(53);  //1.index
+  sayilar.add(25);  //2.index
+
+  sayilar[2]=20; // burada hata almayız 2. index var
+  //sayilar[3]=10; // burada hata alırız çünkü 3.index yok
+  // bu yapıdan sonra hata olduğu satırdan itibaren try duracak ve catch e geçecek
+  try{
+    sayilar[3]=10;
+    print("islem tamam ");
+  }catch(e){
+    print("listenin boyutunu astınız . ");
+  }
+
+
+    Asenkron işlemler aynı anda birden fazla işlem yapmak anlamına gelir .
+    aynı anda birden fazla işlem yapıldığı için daha verimli kod çalışması elde edilir
+
+        Future, async ve await
+        Future : fonksiyon tanımlanmasında fonksiyon isminden önce gelir. methodun asenkron olarak çalişacagini ve await methodu ile
+                  karşılaştığı zaman askıya alacağını bildirir.
+        Async : fonksiyon tanımlanmasından sonra gelir ve asenkron çalışmak istediğimiz yapılarda kullanılır ve aynı zamanda birden
+                  fazla işlem yaptırmak için kullanılır. ornek : dosya işlemleri, internetten veri alirken gibi
+        Await : Sadece async fonksiyonları içerisinde kullanılır .amaç asenkron işlemler yaparken yarım kalan bazı kodlama hataları olabilir
+                  hata oluşturmaması için await kullanılır ve asenkron işlemler içinde kodlamanın bitmesi beklenir.
+
+
+    Future<void> main () async {
+        print("Verilerin Alınması Bekleniyor...");
+        var veri = await veriTabaniVeriAl();   // burada await yazmaz isek program çalışır fakat bu kısımda bir hata alınır
+        print("Veriler alınıyor...");
+        print("Alınan Veri : $veri");
+    }
+
+    Future<String> veriTabaniVeriAl() async{
+        for (var i=1;i<=5;i++){
+            Future.delayed(Duration(seconds: i), () => print("Alinan veri miktari : ${i*20}"));
+        }
+        return Future.delayed(Duration(seconds: 5), () => "Veritabanı Veri Kümesi");
+    }
 
  */
