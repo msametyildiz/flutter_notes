d57969cc6c0a4aa825b6277299d8f123c23fe4e6
msametyildiz <97543064+msametyildiz@users.noreply.github.com>
2023-08-20 17:46:06 +0300

    added the new parts

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,85 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Başlık'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  MyHomePage({Key key, this.title}) : super(key: key);
 
 
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-
-          ],
-        ),
+      body: LayoutBuilder(
+          builder: (BuildContext context,BoxConstraints constraints){
+            if(constraints.maxWidth < 600){
+              return TelefonTasarim();
+            }else{
+              return TabletTasarim();
+            }
+          }
       ),
+
     );
   }
 }
 
-class TabletTasarim extends StatelessWidget {
 
+class TabletTasarim extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Image.asset("resimler/stevejabs@2x.png"),
-          Text("Steve Job", style :TextStyle(fontSize: 20.0),)
-          ],
+          Image.asset("resimler/stevejobs@2x.png"),
+          Text("Steve Jobs",style: TextStyle(fontSize: 30.0),)
+        ],
       ),
     );
   }
 }
 
+class TelefonTasarim extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset("resimler/stevejobs@1x.png"),
+          Text("Steve Jobs",style: TextStyle(fontSize: 20.0),)
+        ],
+      ),
+    );
+  }
+}
