c540a1127a1714d0d1a7e505195cfd7d6ed7690d
msametyildiz <97543064+msametyildiz@users.noreply.github.com>
2023-09-04 22:53:51 +0300

    added the new parts

--- lib/lecture.dart
+++ lib/lecture.dart
@@ -713,6 +713,89 @@ body: Column(
 ),
 
 ----------------------------------------------------             ----------------------------------------------------------------------
+body: Center(
+  child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Container(
+        width: 200,
+        height: 200,
+        decoration: BoxDecoration(
+          color: Colors.yellow,
+          border: Border.all(color: Colors.black, width: 3.0),
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+        ),
+        child: Center(
+          child: Text(
+            "Merhaba",
+            style: TextStyle(fontSize: 24.0),
+          ),
+        ),
+      ),
+      SizedBox(height: 20.0),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Expanded(
+            child: Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+              child: Center(
+                child: Text("Kırmızı", style: TextStyle(color: Colors.white)),
+              ),
+            ),
+          ),
+          SizedBox(width: 10.0),
+          Expanded(
+            child: Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+              child: Center(
+                child: Text("Mavi", style: TextStyle(color: Colors.white)),
+              ),
+            ),
+          ),
+          SizedBox(width: 10.0),
+          Expanded(
+            child: Container(
+              width: 100,
+              height: 100,
+              color: Colors.green,
+              child: Center(
+                child: Text("Yeşil", style: TextStyle(color: Colors.white)),
+              ),
+            ),
+          ),
+        ],
+      ),
+      SizedBox(height: 20.0),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.purple,
+            child: Center(
+              child: Text("Mor", style: TextStyle(color: Colors.white)),
+            ),
+          ),
+          SizedBox(width: 20.0),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.orange,
+            child: Center(
+              child: Text("Turuncu", style: TextStyle(color: Colors.white)),
+            ),
+          ),
+        ],
+      ),
+    ],
+  ),
+),
 
 ----------------------------------------------------             ----------------------------------------------------------------------
 
