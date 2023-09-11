0058855bdc3f33c8b6297ef81ddf0e0db9f4f849
msametyildiz <97543064+msametyildiz@users.noreply.github.com>
2023-09-11 22:28:05 +0300

    added the new parts

--- lib/lecture.dart
+++ lib/lecture.dart
@@ -1234,6 +1234,44 @@ class SecondPage extends StatelessWidget {
     );
   }
 }
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String buttonText = 'Düğmeye Tıkla';
+
+  void changeText() {
+    setState(() {
+      buttonText = 'Tıkladın!';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Düğme Örneği'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                buttonText,
+                style: TextStyle(fontSize: 24.0),
+              ),
+              SizedBox(height: 20.0),
+              RaisedButton(
+                onPressed:
 
 ----------------------------------------------------             ----------------------------------------------------------------------
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String buttonText = 'Düğmeye Tıkla';
+
+  void changeText() {
+    setState(() {
+      buttonText = 'Tıkladın!';
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Başlık'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-
-    var ekranBilgisi=MediaQuery.of(context);
-    final ekranGenisligi=ekranBilgisi.size.width;
-    final ekranYuksekligi=ekranBilgisi.size.height;
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-     s
-    );
-  }
-}
-
-
-
-
-
+        home: Scaffold(
+        appBar: AppBar(
+        title: Text('Düğme Örneği'),
+    ),
+    body: Center(
+    child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+    Text(
+    buttonText,
+    style: TextStyle(fontSize: 24.0),
+    ),
+    SizedBox(height: 20.0),
+    RaisedButton(
+    onPressed:
